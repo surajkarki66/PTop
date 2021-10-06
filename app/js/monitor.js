5f97f5175e92559611026954a289d735d1c20b6e
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-06 22:26:56 +0545

    Finished dynamic system status

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,22 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// Run every 2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = info + "%";
+  });
+
+  // CPU free
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = info + "%";
+  });
+
+  // Uptime
+  document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime());
+}, 2000);
+
 // Set model
 document.getElementById("cpu-model").innerText = cpu.model();
 
@@ -18,3 +34,13 @@ mem.info().then((info) => {
   document.getElementById("mem-total").innerText =
     info.totalMemMb + " " + "Megabytes";
 });
+
+// Shows days, hours, mins, seconds
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
