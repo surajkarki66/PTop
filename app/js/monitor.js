ceb99f7c17cade61f2e02b22ab559c107f4c3b4e
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-08 20:32:20 +0545

    Finished adding tray icon

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,10 +1,18 @@
+const { ipcRenderer } = require("electron");
 const path = require("path");
 const osu = require("node-os-utils");
 const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
-let cpuOverload = 5;
+let cpuOverload;
+let alertFrequency;
+
+// Get settings and values
+ipcRenderer.on("settings:get", (e, settings) => {
+  cpuOverload = +settings.cpuOverload;
+  alertFrequency = +settings.alertFrequency;
+});
 
 // Run every 2 seconds
 setInterval(() => {
@@ -14,10 +22,20 @@ setInterval(() => {
     document.getElementById("cpu-progress").style.width = info + "%";
 
     // Make the progress bar red if overload
-    if (info > cpuOverload) {
+    if (info >= cpuOverload) {
       document.getElementById("cpu-progress").style.background = "red";
     } else {
       document.getElementById("cpu-progress").style.background = "#30c88b ";
+    }
+
+    // Check overloading
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: "CPU Overload",
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+      localStorage.setItem("lastNotified", +new Date());
     }
   });
 
@@ -53,4 +71,28 @@ function secondsToDhms(seconds) {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
   return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+// Send notification
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+// Check how much time has since notification
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotified") === null) {
+    // Store timestamps
+    localStorage.setItem("lastNotified", +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotified")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 const log = require("electron-log");
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require("electron");
+
+const Store = require("./Store");
 
 // Set env
 process.env.NODE_ENV = "development";
 
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
+const isLinux = process.platform === "linux" ? true : false;
 
 let mainWindow;
+let tray;
+
+// Init store and defaults
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -16,6 +32,8 @@ function createMainWindow() {
     height: 500,
     icon: "./assets/icons/icon.png",
     resizable: isDev ? true : false,
+    // show: false,
+    opacity: 0.9,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -32,8 +50,58 @@ function createMainWindow() {
 app.on("ready", () => {
   createMainWindow();
 
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
+  });
+
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("close", (e) => {
+    if (!isLinux && !app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+    return true;
+  });
+
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
+
+  // Create tray
+  tray = new Tray(icon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Quit",
+      click: () => {
+        app.isQuitting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.on("click", () => {
+    if (mainWindow.isVisible() === true) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
+
+  tray.on("right-click", () => {
+    tray.popUpContextMenu(contextMenu);
+  });
+
+  tray.setToolTip("PTop");
+  if (isLinux) {
+    // Make a change to the context menu
+    contextMenu.items[0].checked = false;
+
+    // Call this again for Linux because we modified the context menu
+    tray.setContextMenu(contextMenu);
+  }
+
+  mainWindow.on("ready", () => mainWindow == null);
 });
 
 const menu = [
@@ -55,6 +123,12 @@ const menu = [
       ]
     : []),
 ];
+
+// Set settings
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
+});
 
 app.on("window-all-closed", () => {
   if (!isMac) {
